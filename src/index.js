71493bc0c94add9df7b195f337c27f355b2a9ae7
Vlad Ventura <vladventura@outlook.es>
2019-11-05 11:12:02 -0500

    Pulled out the Icons for the tier emblems, and the colors for these to a separate file

--- src/index.js
+++ src/index.js
@@ -4,16 +4,7 @@ import Summoner from './summoner.js';
 import './index.scss';
 
 // Tier Images
-import IRON from './res/emblems/IRON.png';
-import BRONZE from './res/emblems/BRONZE.png';
-import SILVER from './res/emblems/SILVER.png';
-import GOLD from './res/emblems/GOLD.png';
-import PLATINUM from './res/emblems/PLATINUM.png';
-import DIAMOND from './res/emblems/DIAMOND.png';
-import MASTER from './res/emblems/MASTER.png';
-import GRANDMASTER from './res/emblems/GRANDMASTER.png';
-import CHALLENGER from './res/emblems/CHALLENGER.png';
-import FETCHING from './res/emblems/FETCHING.png';
+import {TIER_EMBLEM_MAP, STATS_COLORS} from './constants/assetmaps';
 
 import NO_ICON from './res/icons/29.png';
 import SearchBar from './components/searchbar.js';
@@ -23,30 +14,6 @@ const API = 'https://na1.api.riotgames.com';
 const SUMMONER_NAME_ENDPOINT = '/lol/summoner/v4/summoners/by-name/';
 const LEAGUE_ID_ENDPOINT = `/lol/league/v4/entries/by-summoner/`;
 const PROXY = 'https://cors-anywhere.herokuapp.com/';
-
-const TIER_EMBLEM_MAP = {
-    BRONZE: BRONZE,
-    IRON: IRON,
-    SILVER: SILVER,
-    GOLD: GOLD,
-    PLATINUM: PLATINUM,
-    DIAMOND: DIAMOND,
-    MASTER: MASTER,
-    GRANDMASTER: GRANDMASTER,
-    CHALLENGER: CHALLENGER
-}
-
-const STATS_COLORS = {
-    BRONZE: '#8F5335',
-    IRON: '#6D6260',
-    SILVER: '#76929C',
-    GOLD: '#D1A035',
-    PLATINUM: '#1ABB5F',
-    DIAMOND: '#6860B7',
-    MASTER: '#724492',
-    GRANDMASTER: '#A11E27',
-    CHALLENGER: '#D9C1C0'
-}
 
 
 class App extends React.Component{
@@ -181,7 +148,7 @@ class App extends React.Component{
                         wins={0}
                         losses={0}
                         leaguePoints={0}
-                        tierEmblem={FETCHING}
+                        tierEmblem={TIER_EMBLEM_MAP['FETCHING']}
                         />
                     )}
                 </div>
