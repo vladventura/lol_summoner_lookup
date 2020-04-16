c93a31f4231968e64a5d9c2e24146adb99f99662
Vlad Ventura <vladventura@outlook.es>
2020-04-16 03:01:43 -0400

    Linting

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const SearchBar = ({onSubmit, formValue, updateSearch}) => {
-    return (
-        <div className='form-box'>
-            <form onSubmit={event => onSubmit(event)}>
-                <label>
-                    <input 
-                        type='text'
-                        value={formValue}
-                        onChange={event => updateSearch(event)}
-                        placeholder="Type the Summoner's name!"
-                    />
-                </label>
-            </form>
-        </div>
-    );
-}
+const SearchBar = ({ onSubmit, formValue, updateSearch }) => {
+  return (
+    <div className="form-box">
+      <form onSubmit={(event) => onSubmit(event)}>
+        <label>
+          <input
+            type="text"
+            value={formValue}
+            onChange={(event) => updateSearch(event)}
+            placeholder="Type the Summoner's name!"
+          />
+        </label>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
