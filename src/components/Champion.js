b06d137be1c27e43c278b07b673300bda0d2f427
Vlad Ventura <vladventura@outlook.es>
2020-04-16 02:47:50 -0400

    Luckily conditional rendering was already here

--- src/components/Champion.js
+++ src/components/Champion.js
@@ -1,20 +1,18 @@
 import React from "react";
 import NO_ICON from "../res/icons/29.png";
 
-const Champion = (props) => {
+const Champion = ({ champ }) => {
   return (
     <div>
       <a
-        href={
-          props.champ ? `https://champion.gg/champion/${props.champ.name}` : "#"
-        }
+        href={champ ? `https://champion.gg/champion/${champ.name}` : "#"}
         rel="noopener noreferrer"
         target="_blank"
       >
         <img
           src={
-            props.champ
-              ? `http://ddragon.leagueoflegends.com/cdn/10.8.1/img/champion/${props.champ.id}.png`
+            champ
+              ? `http://ddragon.leagueoflegends.com/cdn/10.8.1/img/champion/${champ.id}.png`
               : NO_ICON
           }
           alt="Champion Icon"
@@ -24,14 +22,12 @@ const Champion = (props) => {
         <a
           rel="noopener noreferrer"
           href={
-            props.champ
-              ? `https://champion.gg/champion/${props.champ.name}`
-              : "#"
+            champ ? `https://champion.gg/champion/${props.champ.name}` : "#"
           }
           target="_blank"
-          title={props.champ ? props.champ.name : "Loading"}
+          title={champ ? champ.name : "Loading"}
         >
-          {props.champ ? `Highest Mastery: ${props.champ.name}` : "Loading"}
+          {champ ? `Highest Mastery: ${champ.name}` : "Loading"}
         </a>
       </h2>
     </div>
