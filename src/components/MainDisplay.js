a782a97edbfef97b94104fe1b9db06ef2d9f4a23
Vlad Ventura <vladventura@outlook.es>
2020-04-16 03:03:20 -0400

    Linting, but besides that I'm now passing the league object to the Bottom Stats Display

--- src/components/MainDisplay.js
+++ src/components/MainDisplay.js
@@ -1,52 +1,41 @@
-import React from 'react';
+import React from "react";
 
 import NO_ICON from "../res/icons/29.png";
-import { TIER_EMBLEM_MAP } from "../constants/assetmaps";
 
-
-import BottomStatsDisplay from './BottomStatsDisplay';
-import Summoner from './Summoner';
-import Champion from './Champion';
-import {connect} from 'react-redux';
+import BottomStatsDisplay from "./BottomStatsDisplay";
+import Summoner from "./Summoner";
+import Champion from "./Champion";
+import { connect } from "react-redux";
 
 class MainDisplay extends React.Component {
-    render() {
-        const {summoner, league, champion} = this.props;
-        return (
-            <section className='summoner'>
-                <div className='summoner__info'>
-                    <Summoner
-                        summoner={summoner? (summoner.name) : "Loading"}
-                        icon={summoner? (summoner.icon) : NO_ICON}
-                    />
-                    <Champion
-                        champ={champion}
-                    />
-                </div>
-                <div
-                    className='summoner__stats'
-                    style={{ background: league.statsColor }}
-                >
-                    <BottomStatsDisplay 
-                        wins={league.wins}
-                        losses={league.losses}
-                        leaguePoints={league.leaguePoints}
-                        tierEmblem={TIER_EMBLEM_MAP[league.tier]}
-                        tier={league.tier}
-                        rank={league.rank}
-                    />
-                </div>
-            </section>
-        );
-    }
+  render() {
+    const { summoner, league, champion } = this.props;
+    return (
+      <section className="summoner">
+        <div className="summoner__info">
+          <Summoner
+            summoner={summoner ? summoner.name : "Loading"}
+            icon={summoner ? summoner.icon : NO_ICON}
+          />
+          <Champion champ={champion} />
+        </div>
+        <div
+          className="summoner__stats"
+          style={league ? { background: league.statsColor } : {}}
+        >
+          <BottomStatsDisplay league={league} />
+        </div>
+      </section>
+    );
+  }
 }
 
-function mapStateToProps(state){
-    return {
-        summoner: state.summoner,
-        league: state.league,
-        champion: state.champion,
-    }
+function mapStateToProps(state) {
+  return {
+    summoner: state.summoner,
+    league: state.league,
+    champion: state.champion,
+  };
 }
 
 export default connect(mapStateToProps)(MainDisplay);
