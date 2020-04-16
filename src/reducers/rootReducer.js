3f55f8d6133f67e27af378942ccae3bee2a0208d
Vlad Ventura <vladventura@outlook.es>
2020-04-16 03:23:06 -0400

    Tweaked a few things (linting) and added case for the new action

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,33 +1,53 @@
-import { FETCH_SUMMONER, FETCH_LEAGUE, FETCH_MASTERY, FETCH_CHAMPION, SEARCH_SUBMITTED } from '../actions/action_types';
+import {
+  FETCH_SUMMONER,
+  FETCH_LEAGUE,
+  FETCH_MASTERY,
+  FETCH_CHAMPION,
+  SEARCH_SUBMITTED,
+  SEARCH_COMPLETE,
+} from "../actions/action_types";
 
 var initState = {
-    summoner: null,
-    league: null,
-    mastery: null,
-    champion: null,
+  summoner: null,
+  league: null,
+  mastery: null,
+  champion: null,
 };
 
-export function rootReducer(state = initState, action){
-    switch(action.type){
-        case FETCH_SUMMONER: return {
-            ...state,
-            summoner: action.payload,
-        };
-        case FETCH_LEAGUE: return {
-            ...state,
-            league: action.payload,
-        };
-        case FETCH_MASTERY: return {
-            ...state,
-            mastery: action.payload
-        };
-        case FETCH_CHAMPION: return {
-            ...state,
-            champion: action.payload
-        }
-        case SEARCH_SUBMITTED: return initState;
-        default: return state;
-    };
+export function rootReducer(state = initState, action) {
+  switch (action.type) {
+    case SEARCH_SUBMITTED:
+      return {
+          ...initState,
+          loaded: false
+      };
+    case FETCH_SUMMONER:
+      return {
+        ...state,
+        summoner: action.payload,
+      };
+    case FETCH_LEAGUE:
+      return {
+        ...state,
+        league: action.payload,
+      };
+    case FETCH_MASTERY:
+      return {
+        ...state,
+        mastery: action.payload,
+      };
+    case FETCH_CHAMPION:
+      return {
+        ...state,
+        champion: action.payload,
+      };
+    case SEARCH_COMPLETE: return {
+        ...state,
+        loaded: true
+    }
+    default:
+      return state;
+  }
 }
 
 export default rootReducer;
