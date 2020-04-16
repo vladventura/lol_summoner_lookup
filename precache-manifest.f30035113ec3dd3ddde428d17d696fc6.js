405e886768687c6ccc73b413105c8e445e634351
Vlad Ventura <vladventura@outlook.es>
2020-04-15 23:03:30 -0400

    Updates

---
+++ precache-manifest.f30035113ec3dd3ddde428d17d696fc6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543f8023db817f5a50902f49b9580e9d",
+    "url": "/lolsearch/index.html"
+  },
+  {
+    "revision": "86d8294162f6a0793c84",
+    "url": "/lolsearch/static/css/main.27260618.chunk.css"
+  },
+  {
+    "revision": "afd34bde68ab183bc618",
+    "url": "/lolsearch/static/js/2.e5369a2e.chunk.js"
+  },
+  {
+    "revision": "920db8be36c2fb274336fe2fa13fb072",
+    "url": "/lolsearch/static/js/2.e5369a2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86d8294162f6a0793c84",
+    "url": "/lolsearch/static/js/main.b2c067c2.chunk.js"
+  },
+  {
+    "revision": "c9e3339322a39764548f",
+    "url": "/lolsearch/static/js/runtime-main.b44ba1c1.js"
+  },
+  {
+    "revision": "94b84a7df00eeb73d53c55fbb116ce75",
+    "url": "/lolsearch/static/media/29.94b84a7d.png"
+  },
+  {
+    "revision": "39964f7f7440521c5d53a63cbeed96ff",
+    "url": "/lolsearch/static/media/BRONZE.39964f7f.png"
+  },
+  {
+    "revision": "4aaf9754594ca11cbbe6a146ffb0eaa1",
+    "url": "/lolsearch/static/media/CHALLENGER.4aaf9754.png"
+  },
+  {
+    "revision": "c901832766fb4f856d1f9603208c6761",
+    "url": "/lolsearch/static/media/DIAMOND.c9018327.png"
+  },
+  {
+    "revision": "60fee29e0bf56291ff541eec77d81fff",
+    "url": "/lolsearch/static/media/FETCHING.60fee29e.png"
+  },
+  {
+    "revision": "19be519b965d28bf5af7cbb6b7f2471a",
+    "url": "/lolsearch/static/media/GOLD.19be519b.png"
+  },
+  {
+    "revision": "d0d4b62f7415cf0ca06d60ce1ae36744",
+    "url": "/lolsearch/static/media/GRANDMASTER.d0d4b62f.png"
+  },
+  {
+    "revision": "1a6b4669f3cb62c2edf32971da0b146e",
+    "url": "/lolsearch/static/media/IRON.1a6b4669.png"
+  },
+  {
+    "revision": "4156f1c160999db19a69d5bb0e2aaca7",
+    "url": "/lolsearch/static/media/MASTER.4156f1c1.png"
+  },
+  {
+    "revision": "86ac5170561afe99a457dccd664eaf72",
+    "url": "/lolsearch/static/media/PLATINUM.86ac5170.png"
+  },
+  {
+    "revision": "3a4aa1a5e576ae0ffb79db1f7505ba37",
+    "url": "/lolsearch/static/media/SILVER.3a4aa1a5.png"
+  }
+]);
